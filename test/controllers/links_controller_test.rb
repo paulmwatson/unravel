d8c7bf1947b66d00928b0dff6b8bdf8089cacaf9
paulmwatson <github.com@paulmwatson.com>
2020-06-19 16:19:18 +0200

    Basic tests

--- test/controllers/links_controller_test.rb
+++ test/controllers/links_controller_test.rb
@@ -3,13 +3,23 @@
 require 'test_helper'
 
 class LinksControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    host! ENV['HOST']
+  end
+
   test 'should get show' do
-    get root_url
+    get root_path
     assert_response :success
   end
 
-  test 'should get show speedcurve.net' do
-    get root_url(url: 'https://speedcurve.net/')
+  test 'should get show with url' do
+    get root_path(url: 'https://speedcurve.com/') # Note: Has `/` as `location` in header, used to cause infinite loop
+    assert_response :success
+
+    get root_path(url: 'http://google.com/')
+    assert_response :success
+
+    get root_path(url: 'https://click.linksynergy.com/deeplink?id=nOD%2FrLJHOac&mid=24542&u1=verge&murl=https%3A%2F%2Fwww.microsoft.com%2Fstore%2Fr%2F9NBNGW0R7KVH')
     assert_response :success
   end
 end
