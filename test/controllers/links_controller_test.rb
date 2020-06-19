fb51d294954a03185850bb45f8e0a5e2e910c216
paulmwatson <github.com@paulmwatson.com>
2020-06-19 15:26:51 +0200

    Fix case where location in header is a slash (/) and goes into an infinite loop

--- test/controllers/links_controller_test.rb
+++ test/controllers/links_controller_test.rb
@@ -1,9 +1,15 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class LinksControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get links_show_url
+  test 'should get show' do
+    get root_url
     assert_response :success
   end
 
+  test 'should get show speedcurve.net' do
+    get root_url(url: 'https://speedcurve.net/')
+    assert_response :success
+  end
 end
